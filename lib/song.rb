51f6d48734a0781bb97b278341195d7dcbd3f124
Becky Rosa <becky_m_rosa@yahoo.com>
2021-03-29 23:36:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,18 @@
   class Song
     attr_accessor :name, :artist, :genre
     @@count = 0
+    @@genres = []
+    @@artists = []
 
-    def initialize
-      @@count += 1
+    def initialize(name, artist, genre)
+      @@count +=1
+
     end
 
     def self.count
       @@count
     end
 
-    def initialize
-      @@genres = []
-    end
-
-    def self.unique
-      @@genres
-    end
 
 
   end
