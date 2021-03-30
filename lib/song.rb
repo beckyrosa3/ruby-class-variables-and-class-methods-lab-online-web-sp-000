43e1be9092fde87863482cff33cdef96d6944fbc
Becky Rosa <becky_m_rosa@yahoo.com>
2021-03-30 00:11:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@
     def self.genre_count
       genre_count = {}
       @@genres.each do |gen|
-        !genre_count.key?(gen)
+        if genre_count.key?(gen)
         genre_count[gen] = 0
       end
         genre_count[gen] += 1
@@ -38,9 +38,15 @@
       end
     end
 
-    #def artist_count
-    #  artist_count = {}
-    #  @@artists.each do |art|
-    #    if !artist_count.key?(art)
-    #      artist_count[art] = 0
-    #    end
+    def artist_count
+      artist_count = {}
+      @@artists.each do |art|
+        if !artist_count.key?(art)
+          artist_count[art] = 0
+        end
+          artist_count[art] += 1
+        end
+          artist_count
+        end
+      end
+    end
